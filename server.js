c632ed4b6c9866ca7dccff8fd6cb4498906d81df
James Swineson <jamesswineson@gmail.com>
2016-05-27 09:15:03 +0800

    add more useless comment

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ router.use(function (req, res, next)
 });
 
 // test route to make sure everything is working (accessed at GET
-// http://localhost:8080/api)
+// http://localhost:3000/api)
 router.get('/', function (req, res)
 {
 	res.json(
